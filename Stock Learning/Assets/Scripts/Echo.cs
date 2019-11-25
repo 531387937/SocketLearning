de44669a390b920634f775b1a5cf488f5e4f238c
Qizi <531387937@qq.com>
2019-11-26 01:22:56 +0800

    异步发送、异步连接

--- Stock Learning/Assets/Scripts/Echo.cs
+++ Stock Learning/Assets/Scripts/Echo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
 using UnityEngine.UI;
+using System;
 public class Echo : MonoBehaviour
 {
     //定义套接字
@@ -10,11 +11,15 @@ public class Echo : MonoBehaviour
 
     public InputField inputField;
     public Text text;
-    
+
+    byte[] readBuff = new byte[1024];
+    string recvStr = "";
+
     public void Connection()
     {
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        socket.Connect("192.168.1.9", 8888);
+        //socket.Connect("192.168.1.9", 8888);
+        socket.BeginConnect("127.0.0.1", 8888, ConnectCallback, socket);
     }
 
     public void Send()
@@ -23,10 +28,45 @@ public class Echo : MonoBehaviour
         byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
         socket.Send(sendBytes);
 
-        byte[] readBuff = new byte[1024];
-        int count = socket.Receive(readBuff);
-        string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+        //byte[] readBuff = new byte[1024];
+        //int count = socket.Receive(readBuff);
+        //string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+        //text.text = recvStr;
+        //socket.Close();
+    }
+
+    private void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            socket.EndConnect(ar);
+            Debug.Log("Socket Connect Succ");
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log("Socket Connect fail" + ex.ToString());
+        }
+    }
+
+    private void ReceiveCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            int count = socket.EndReceive(ar);
+            recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch(SocketException ex)
+        {
+            Debug.Log("Socket Receive fail" + ex.ToString());
+        }
+    }
+    public void Update()
+    {
+
         text.text = recvStr;
-        socket.Close();
     }
 }
